2e019b43aa866ada15a4ed25cd3d7885ac10cc4a
woodenclock <luckyisland3710@gmail.com>
2023-10-09 19:48:31 +0800

    Create EntryList class

---
+++ src/main/java/seedu/wildwatch/operation/EntryList.java
@@ -0,0 +1,8 @@
+package seedu.wildwatch.operation;
+
+import seedu.wildwatch.entry.Entry;
+import java.util.ArrayList;
+
+public class EntryList {
+    private static ArrayList<Entry> tasks = new ArrayList<>();   //Keeps track of all Task Instances made
+}
