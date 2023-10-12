e62a99b83b1a86695fdd408c40fa647c6d55c395
woodenclock <luckyisland3710@gmail.com>
2023-10-12 20:41:34 +0800

    Update JUnit Testing

--- src/test/java/seedu/wildwatch/operation/UiTest.java
+++ src/test/java/seedu/wildwatch/operation/UiTest.java
@@ -3,7 +3,6 @@ package seedu.wildwatch.operation;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UiTest {
