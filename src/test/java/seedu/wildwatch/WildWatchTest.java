9219e532962e466897da973e62b635128a9537d7
woodenclock <luckyisland3710@gmail.com>
2023-10-12 19:54:08 +0800

    Update WildWatchTest.java

--- src/test/java/seedu/wildwatch/WildWatchTest.java
+++ src/test/java/seedu/wildwatch/WildWatchTest.java
@@ -5,4 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class WildWatchTest {
+
+    @Test
+    public void isDateValid() {
+        assertTrue(true);
+    }
 }
