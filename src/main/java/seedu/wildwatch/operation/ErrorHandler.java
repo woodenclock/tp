62cdd84658d7065539c5f991671b99913c12e384
woodenclock <luckyisland3710@gmail.com>
2023-10-16 16:03:05 +0800

    Update Assertions and Logging for ErrorHandler.java

--- src/main/java/seedu/wildwatch/operation/ErrorHandler.java
+++ src/main/java/seedu/wildwatch/operation/ErrorHandler.java
@@ -2,6 +2,7 @@ package seedu.wildwatch.operation;
 
 import java.time.format.DateTimeParseException;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 import seedu.wildwatch.command.HelpCommand;
 import seedu.wildwatch.entry.EntryList;
@@ -16,6 +17,7 @@ import seedu.wildwatch.exception.IncorrectInputException;
 
 public class ErrorHandler {
     private static final int DEFAULT_NUMBER_INPUT = -3710; //Number never input during normal use of WildWatch
+    private static final Logger LOGGER = Logger.getLogger(ErrorHandler.class.getName());
     public static void handleError(String inputBuffer) {
         boolean validInput = false;
         try {
@@ -23,22 +25,30 @@ public class ErrorHandler {
             Parser.entryManager(inputBuffer, false);
             validInput = true;
         } catch (EmptyInputException exception) {
+            LOGGER.warning("Received an empty input.");
             Ui.emptyDescriptionMessagePrinter(null);
         } catch (EmptyAddException exception){
+            LOGGER.warning("Received an empty add input.");
             Ui.emptyDescriptionMessagePrinter("add");
         } catch (EmptyDeleteException exception) {
+            LOGGER.warning("Received an empty delete input.");
             Ui.emptyDescriptionMessagePrinter("delete");
         } catch (EmptyListException exception) {
+            LOGGER.info("List is empty.");
             Ui.emptyListMessagePrinter();
             validInput = true;
         } catch (EntryNotFoundException exception) {
+            LOGGER.warning("Queried entry not found.");
             Ui.entryNotFoundMessagePrinter();
             validInput = true;
         } catch (UnknownInputException exception) {
+            LOGGER.warning("Unknown input received.");
             Ui.unknownInputMessagePrinter();
         } catch (UnknownDateFormatException | DateTimeParseException exception) {
+            LOGGER.warning("Date is invalid.");
             Ui.invalidDateMessagePrinter();
         } catch (IncorrectInputException exception) {
+            LOGGER.warning("Command is invalid.");
             Ui.incorrectInputMessagePrinter();
         } finally {
             if (!validInput) {
@@ -50,7 +60,7 @@ public class ErrorHandler {
 
     public static void checkError(String inputBuffer) throws EmptyInputException, EmptyAddException,
             EmptyListException, EmptyDeleteException, EntryNotFoundException, UnknownDateFormatException {
-
+        assert inputBuffer != null : "Input buffer should not be null.";
         Scanner bufferScanner = new Scanner(inputBuffer);   //Scanner for the buffer
         String firstWord;                                   //First word of input
         int numberInput = DEFAULT_NUMBER_INPUT;
