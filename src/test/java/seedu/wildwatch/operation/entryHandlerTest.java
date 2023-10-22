b8b831fd2eee00070db0641f2d18c427da06ae7e
woodenclock <luckyisland3710@gmail.com>
2023-10-22 20:17:55 +0800

    Update HandlerTest.java

--- src/test/java/seedu/wildwatch/operation/entryHandlerTest.java
+++ src/test/java/seedu/wildwatch/operation/entryHandlerTest.java
@@ -1,11 +1,53 @@
 package seedu.wildwatch.operation;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import seedu.wildwatch.exception.IncorrectInputException;
+import seedu.wildwatch.exception.UnknownInputException;
 
-public class entryHandlerTest {
+public class EntryHandlerTest {
+
+    @BeforeEach
+    public void setUp() {
+        // Set up necessary mocks, initial data, etc.
+    }
+
     @Test
-    public void isMainValid() {
-        assertTrue(true);
+    public void testAddEntryFromFile() {
+        String input = "add D/02-03-23 S/Annam Leaf Turtle N/Ariel_2 R/Injured left flipper";
+        assertDoesNotThrow(() -> EntryHandler.handleEntry(input, true));
+    }
+
+    @Test
+    public void testAddEntryNotFromFile() {
+        String input = "add D/02-03-23 S/Annam Leaf Turtle N/Ariel_2 R/Injured left flipper";
+        assertDoesNotThrow(() -> EntryHandler.handleEntry(input, false));
+    }
+
+    @Test
+    public void testDeleteEntryWithValidNumber() {
+        String input = "delete 1"; // Assuming 1 is a valid entry number.
+        assertDoesNotThrow(() -> EntryHandler.handleEntry(input, false));
+    }
+
+    @Test
+    public void testDeleteEntryWithInvalidNumber() {
+        String input = "delete -5"; // Negative numbers should be invalid.
+        assertThrows(IncorrectInputException.class, () -> EntryHandler.handleEntry(input, false));
+    }
+
+    @Test
+    public void testListEntry() {
+        String input = "list";
+        assertDoesNotThrow(() -> EntryHandler.handleEntry(input, false));
+    }
+
+    @Test
+    public void testUnknownCommand() {
+        String input = "unknownCommand";
+        assertThrows(UnknownInputException.class, () -> EntryHandler.handleEntry(input, false));
     }
 }
+
