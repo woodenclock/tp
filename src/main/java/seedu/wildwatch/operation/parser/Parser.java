f9a5218d191bceec121f5d57b12ecda221a36c4e
lctxct <caitinglien@hotmail.com>
2023-10-30 01:03:18 +0800

    Remove unused exceptions

--- src/main/java/seedu/wildwatch/operation/parser/Parser.java
+++ src/main/java/seedu/wildwatch/operation/parser/Parser.java
@@ -2,9 +2,8 @@ package seedu.wildwatch.operation.parser;
 
 import seedu.wildwatch.command.Command;
 import seedu.wildwatch.exception.IncorrectInputException;
-import seedu.wildwatch.exception.UnknownInputException;
 
 public interface Parser<T extends Command> {
 
-    T parse(String input) throws IncorrectInputException, UnknownInputException;
+    T parse(String input) throws IncorrectInputException;
 }
