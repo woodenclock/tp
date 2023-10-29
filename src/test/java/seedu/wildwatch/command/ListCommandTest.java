65c3f395560267925a4062ef616a8f407780d2d7
tangzhenen <tangzhenen2000@gmail.com>
2023-10-29 15:58:16 +0800

    Add list command test

---
+++ src/test/java/seedu/wildwatch/command/ListCommandTest.java
@@ -0,0 +1,62 @@
+package seedu.wildwatch.command;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static seedu.wildwatch.entry.EntryList.addEntry;
+import static seedu.wildwatch.entry.EntryList.clearEntry;
+
+import seedu.wildwatch.entry.Entry;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+class ListCommandTest {
+    private final PrintStream originalOut = System.out;
+    private final ByteArrayOutputStream mockOutput = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void redirectSystemOut() {
+        System.setOut(new PrintStream(mockOutput));
+    }
+
+    @AfterEach
+    void restoreSystemOut() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void listEntry() {
+        clearEntry();
+        addEntry(new Entry("28-10-23", "Lion", "Simba", "This is Simba."));
+        addEntry(new Entry("28-10-23", "Tiger", "Tigger", "This is Tigger."));
+
+        ListCommand.listEntry();
+        String consoleOutput = mockOutput.toString();
+
+        String expectedOutput = "1.Date: [28-10-2023] | Species: [Lion] | Name: [Simba] | Remark: [This is Simba.]"
+                + "\n"
+                + "2.Date: [28-10-2023] | Species: [Tiger] | Name: [Tigger] | Remark: [This is Tigger.]"
+                + "\n"
+                + "Now you have 2 entries in the list."
+                + "\n";
+
+        consoleOutput = consoleOutput.replace(System.lineSeparator(), "\n");
+
+        assertEquals(expectedOutput, consoleOutput);
+    }
+
+    @Test
+    void listEntryWithZeroEntry() {
+        clearEntry();
+        ListCommand.listEntry();
+        String consoleOutput = mockOutput.toString();
+        String expectedOutput = "Now you have 0 entries in the list."
+                + "\n";
+        consoleOutput = consoleOutput.replace(System.lineSeparator(), "\n");
+        assertEquals(expectedOutput, consoleOutput);
+    }
+}
