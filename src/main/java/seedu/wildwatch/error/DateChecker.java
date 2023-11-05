69e87ff6dbb49dfebbe3118724aff9e2d9f42499
woodenclock <luckyisland3710@gmail.com>
2023-11-05 23:15:54 +0800

    Update isDateValid to identify invalid dates, which takes into account the leap day, days in different months.

--- src/main/java/seedu/wildwatch/error/DateChecker.java
+++ src/main/java/seedu/wildwatch/error/DateChecker.java
@@ -1,17 +1,30 @@
 //@@woodenclock
 package seedu.wildwatch.error;
 
-import java.util.regex.Pattern;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+
 public class DateChecker {
-    private static final Pattern DATE_PATTERN = Pattern.compile(
-            "(0[1-9]|[1-2][0-9]|3[0-1])-(0[1-9]|1[0-2])-\\d{2}"   // dd-mm-yy
-    );
+    private static final DateTimeFormatter DATE_FORMATTER =
+            DateTimeFormatter.ofPattern("dd-MM-uuuu").withResolverStyle(ResolverStyle.STRICT);
 
     public static boolean isDateValid(String date) {
-        date = date.trim();
-        if (DATE_PATTERN.matcher(date).matches()) {
+        try {
+            // Split the date string into parts
+            String[] parts = date.trim().split("-");
+
+            // Prepend "20" to the year part
+            String year = "20" + parts[2];
+
+            // Reconstruct the date string with the updated year
+            String updatedDate = parts[0] + "-" + parts[1] + "-" + year;
+
+            LocalDate.parse(updatedDate.trim(), DATE_FORMATTER);
             return true;
+        } catch (DateTimeParseException e) {
+            return false;
         }
-        return false;
     }
 }
