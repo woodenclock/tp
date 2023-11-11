981b1aa935f16ab03e684714f9ae9e4b7d712f06
imaginarys96 <imaginarys96@gmail.com>
2023-11-11 14:38:34 +0800

    fixed gradle import error

--- src/test/java/seedu/wildwatch/command/SummaryCommandTest.java
+++ src/test/java/seedu/wildwatch/command/SummaryCommandTest.java
@@ -11,6 +11,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class SummaryCommandTest {
     private Entry testEntry;
